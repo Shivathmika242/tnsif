76c1d1d830222ed907517dcea8aab3dca4ce764f
Shivathmika242 <vuthurshivathmika@gmail.com>
2025-09-12 19:30:14 +0530

    Palindrome number

---
+++ TNS/src/javatraining/PalindromeNumber.java
@@ -0,0 +1,28 @@
+package javatraining;
+import java.util.Scanner;
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter a number:");
+		int num= sc.nextInt();
+		int original = num;
+		int reversed =0;
+		
+		while(num>0){
+			int digit = num % 10;
+			reversed = reversed *10 + digit;
+			num/=10; 	
+			
+	}
+		if(original==reversed) {
+			System.out.println(original + " is a palindrome number");
+			
+		}
+		else {
+			System.out.println(original + " is not a palindrome number");
+			
+		}
+}
+}
