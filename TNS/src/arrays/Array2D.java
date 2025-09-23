20db3f3876104dfda5b24ee353bc4b49e59b876c
Shivathmika242 <vuthurshivathmika@gmail.com>
2025-09-23 21:57:47 +0530

    Constructors

--- TNS/src/arrays/Array2D.java
+++ TNS/src/arrays/Array2D.java
@@ -2,9 +2,8 @@ package arrays;
 
 public class Array2D {
 
-	public static void main(String[] args) 
-	{
-		 
+	public static void main(String[] args) {
+		
 
 	}
 
--- TNS/src/constructors/DefaultConstructor.java
+++ TNS/src/constructors/DefaultConstructor.java
@@ -1,4 +1,4 @@
-package javatraining;
+package constructors;
 
 public class DefaultConstructor {
 
