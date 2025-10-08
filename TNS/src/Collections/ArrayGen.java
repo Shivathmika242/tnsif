455bd4547dc49916bd5094ec2d4c7f17160da48d
Shivathmika242 <vuthurshivathmika@gmail.com>
2025-10-08 19:08:50 +0530

    programs

---
+++ TNS/src/Collections/ArrayGen.java
@@ -0,0 +1,12 @@
+package Collections;
+import java.util.ArrayList;
+public class ArrayGen {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> l = new ArrayList<Integer>();
+		l.add(101);
+
+	}
+
+}
